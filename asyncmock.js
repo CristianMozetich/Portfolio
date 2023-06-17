ee62459bebf6a595dcfe311c04c681872475f72a
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-17 22:29:49 +0100

    probando img

--- asyncmock.js
+++ asyncmock.js
@@ -1,28 +1,28 @@
 
 export const proyectos = [ {
     id: 1,
-    img: "./public/assets/img/magenta.png",
+    img: "/src/assets/Img/magenta.png",
     name: "Magenta",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus.",
     web: "https://cristianmozetich.github.io/Magenta/",
 },
 {
     id: 2,
-    img: "./public/assets/img/houseofmusic.png",
+    img: "/src/assets/img/houseofmusic.png",
     name: "House Of Music",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus.",
     web: "https://cristianmozetich.github.io/ProyectoFinal-Mozetich/",
 },
 {
     id: 3,
-    img: "./public/assets/img/art&wine.png",
+    img: "/src/assets/img/art&wine.png",
     name: "Art & Wine",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus.",
     web: "https://43200-react.netlify.app/",
 },
 {
     id: 4,
-    img: "./public/assets/img/tateti.png",
+    img: "/src/assets/img/tateti.png",
     name: "Tic Tac Toe ",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus.",
     web: "https://superlative-raindrop-91350e.netlify.app/",
