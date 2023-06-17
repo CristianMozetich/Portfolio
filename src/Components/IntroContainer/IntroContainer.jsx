6a979125e20eb89004a3a41b449454e3dcf9b4db
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-17 22:02:50 +0100

    probando las img

--- src/Components/IntroContainer/IntroContainer.jsx
+++ src/Components/IntroContainer/IntroContainer.jsx
@@ -6,7 +6,7 @@ const IntroContainer = () => {
   return (
     <div className='intro_container'>
         <Intro/>
-        <img src={"./public/assets/Img/portfolio1.jpg"} alt="" />
+        <img src={"./assets/Img/portfolio1.jpg"} alt="" />
     </div>
   )
 }
