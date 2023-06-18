7d68bfa3cf7c2aa13023db147a8150453c9fbc62
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-18 18:48:03 +0100

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 import IntroContainer from './Components/IntroContainer/IntroContainer'
 import Proyects from './Components/Proyects/Proyects'
 
+
 function App() {
+
 
   return (
     <>
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,10 +7,10 @@ const Intro = () => {
         <h1>Hi I'm Cristian &#x1F44B;</h1>
         <p>I'm a <mark>web developer</mark> , <mark>drummer</mark> , and <mark>physical education teacher</mark> based in Dublin. Here you can find all relevant links and a selection of my latest projects</p>
         <li>
-            <a href="https://github.com/CristianMozetich" target='_blank'><i class="fa-brands fa-github"></i>Git Hub</a>
-            <a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'><i class="fa-brands fa-linkedin-in"></i>LinkedIn</a>
-            <a href="https://wa.me/353830941442" target='_blank'><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
-            <a href="#"><i class="fa-regular fa-envelope"></i>Email</a>
+            <a href="https://github.com/CristianMozetich" target='_blank'><i className="fa-brands fa-github"></i>Git Hub</a>
+            <a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'><i className="fa-brands fa-linkedin-in"></i>LinkedIn</a>
+            <a href="https://wa.me/353830941442" target='_blank'><i className="fa-brands fa-whatsapp"></i>Whatsapp</a>
+            <a href="#"><i className="fa-regular fa-envelope"></i>Email</a>
         </li>
     </div>
   )
