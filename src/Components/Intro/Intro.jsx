bb801d84e9318ee7dfbb936661a8508849da0519
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-18 18:25:11 +0100

    portfolio links

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -6,9 +6,12 @@ const Intro = () => {
     <div className='intro_desc'>
         <h1>Hi I'm Cristian &#x1F44B;</h1>
         <p>I'm a <mark>web developer</mark> , <mark>drummer</mark> , and <mark>physical education teacher</mark> based in Dublin. Here you can find all relevant links and a selection of my latest projects</p>
-        <button><a href="https://github.com/CristianMozetich" target='_blank'>Git Hub</a></button>
-        <button><a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'>LinkedIn</a></button>
-        <button><a href="https://wa.me/353830941442" target='_blank'>Whatsapp</a></button>
+        <li>
+            <a href="https://github.com/CristianMozetich" target='_blank'><i class="fa-brands fa-github"></i>Git Hub</a>
+            <a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'><i class="fa-brands fa-linkedin-in"></i>LinkedIn</a>
+            <a href="https://wa.me/353830941442" target='_blank'><i class="fa-brands fa-whatsapp"></i>Whatsapp</a>
+            <a href="#"><i class="fa-regular fa-envelope"></i>Email</a>
+        </li>
     </div>
   )
 }
