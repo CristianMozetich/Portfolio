2b77b81636ae6bb829750743ab9e2b1027a327f3
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-19 18:53:14 +0100

    modoclaro/oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import './App.css'
-import IntroContainer from './Components/IntroContainer/IntroContainer'
-import Proyects from './Components/Proyects/Proyects'
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import IntroContainer from './Components/IntroContainer/IntroContainer';
+import Proyects from './Components/Proyects/Proyects';
 
 function App() {
+  const [modoOscuro, setModoOscuro] = useState(false);
 
+  useEffect(() => {
+    if (modoOscuro) {
+      document.body.classList.add('modo-oscuro');
+    } else {
+      document.body.classList.remove('modo-oscuro');
+    }
+  }, [modoOscuro]);
+
+  const handleClick = () => {
+    setModoOscuro(!modoOscuro);
+  }
 
   return (
-    <>
-      <IntroContainer/>
-      <Proyects/>
-    </>
-  )
+    <div className={`App ${modoOscuro ? 'modo-oscuro' : 'modo-claro'}`}>
+      <button onClick={handleClick}>Cambiar Modo</button>
+      <IntroContainer />
+      <Proyects />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
