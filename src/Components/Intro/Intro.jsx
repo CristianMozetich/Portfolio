3f2b08d7168ffb85b7d7e7a0a42ea7d80ebadcda
Cristian Mozetich <cristianmozetich@gmail.com>
2023-06-28 21:13:21 +0100

    correciones

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
   return (
     <div className='intro_desc'>
         <h1 className='titulo'>Hi, I'm Cristian &#x1F44B;</h1>
-        <p>I'm a <mark>web developer</mark> , <mark>drummer</mark> , and <mark>physical education teacher</mark> based in Dublin. Here you can find all relevant links and a selection of my latest projects</p>
+        <p>I'm a <mark>web developer</mark>, <mark>drummer</mark>, and <mark>physical education teacher</mark> based in Dublin. Here you can find all relevant links and a selection of my latest projects</p>
         <li>
             <a href="https://github.com/CristianMozetich" target='_blank'><i className="fa-brands fa-github"></i>Git Hub</a>
             <a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'><i className="fa-brands fa-linkedin-in"></i>LinkedIn</a>
