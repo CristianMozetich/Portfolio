064686ae972332687a34df013778d7fa8d7a058a
Cristian Mozetich <cristianmozetich@gmail.com>
2023-07-18 19:42:37 +0100

    verde mejor visual

--- asyncmock.js
+++ asyncmock.js
@@ -2,7 +2,6 @@
 export const proyectos = [
 {
     id: 1,
-    //img: "./assets/Img/imc2.jpg",
     name: "IMC",
     desc: "App para calcular tu índice de masa corporal (IMC) ingresando tu peso y altura.",
     web: "https://indice-m-corp.netlify.app/",
@@ -11,7 +10,6 @@ export const proyectos = [
 },
 {
     id: 2,
-    //img: "./assets/Img/art_wine.jpg",
     name: "Art & Wine",
     desc: "App que simula una tienda de vinos, puedes agregar al carrito y comprar.",
     web: "https://art-wine.netlify.app/",
@@ -20,7 +18,6 @@ export const proyectos = [
 },
 {
     id: 3,
-    //img: "./assets/Img/tateti.jpg",
     name: "Tic Tac Toe ",
     desc: "Juego Tic-Tac-Toe",
     web: "https://ta-te-ti-91.netlify.app/",
@@ -29,16 +26,14 @@ export const proyectos = [
 },
 {
     id: 4,
-    //img: "./assets/Img/music.jpg",
     name: "House Of Music",
-    desc: "Tienda de instrumentos músicales, puedes agregar al carrito, comprar y disfrutar un blog de lectura.",
+    desc: "Tienda de instrumentos músicales, puedes agregar al carrito y comprar.",
     web: "https://house-of-music.netlify.app/",
     year: "2023",
     tech: "Html - Css - JavaScript - Bootstrap",
 },
 {
     id: 5,
-    //img: "./assets/Img/magenta.jpg",
     name: "Magenta",
     desc: "Página web de un comercio dedicado a la fabricación, venta de alianzas y relojes.",
     web: "https://cristianmozetich.github.io/Magenta/",
