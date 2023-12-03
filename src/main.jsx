dca09b8fa3e1f46941a33d5f6120986aeb544d84
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-03 15:52:08 +0000

    cambios en la importacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 // main.tsx or main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import {NextUIProvider} from '@nextui-org/react'
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
