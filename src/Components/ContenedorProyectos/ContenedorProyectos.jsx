4cb01113e1a4c46d44911e8d3cf12fdec8afd230
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-03 13:56:38 +0000

    portfolio des

--- src/Components/ContenedorProyectos/ContenedorProyectos.jsx
+++ src/Components/ContenedorProyectos/ContenedorProyectos.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Proyects from '../Proyects/Proyects'
-import './ContenedorProyectos.css'
 
 const ContenedorProyectos = () => {
   return (
     <>
-      <h1 className='titulo_proyectos'>Proyectos</h1>
       <Proyects/>
     </>
   )
