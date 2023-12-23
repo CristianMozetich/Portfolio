233186ff7b0860998a029e4fcb3edb939cf79cd4
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-23 20:25:16 +0000

    actualizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import IntroContainer from './Components/IntroContainer/IntroContainer';
 import Proyects from './Components/Proyects/Proyects';
 import './App.css';
 import EducarProyect from './Components/EducarProyect/EducarProyect';
+import LinksMedia from './Components/LinksMedia/LinksMedia';
 
 function App() {
   const [modoLight, setModoLight] = useState(false);
@@ -22,6 +23,7 @@ function App() {
   return (
     <div className={`App ${modoLight ? 'modo-light' : 'modo-navy'}`}>
       <button className='btn' onClick={handleClick}><i className="fa-solid fa-circle-half-stroke"></i>Navy/Light</button>
+      <LinksMedia/>
       <IntroContainer />
       <EducarProyect/>
       <Proyects />
---
+++ src/Components/LinksMedia/LinksMedia.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LinksMedia.css'
+
+const LinksMedia = () => {
+  return (
+    <div className='LinksMedia'>
+        <a href="https://github.com/CristianMozetich" target='_blank'><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/cristian-mozetich-135086256/" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.instagram.com/cristianmozetich/" target='_blank'><i class="fa-brands fa-instagram"></i></a>
+        <div className='linea-vertical'></div>
+    </div>
+  )
+}
+export default LinksMedia
