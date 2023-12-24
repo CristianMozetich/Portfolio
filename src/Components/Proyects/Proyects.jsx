b6c4accd98361ebbc8becbf17d0d5d0c9bd5aaed
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-24 11:58:54 +0000

    actualizaciones

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -6,38 +6,38 @@ import './Proyects.css'
 const Proyects = () => {
   return (
     <>
-    <div className='proyectos'>
-      <h2>Proyectos</h2>
-    </div>
-    <div className='contenedor_proyectos'>
-      {
-      proyectos.map((proyecto) => (
-          <article className='tarjeta_proyectos' key={proyecto.id}>
-            <div>
-              <a href={proyecto.web} target='_blank'>
-              <i class="fa-solid fa-arrow-up-right-from-square"></i>
-              <h2 className='title'>{proyecto.name}</h2>
-              </a>
-            </div>
+      <div className='proyectos'>
+        <h2>Proyectos</h2>
+      </div>
+      <div className='contenedor_proyectos'>
+        {
+        proyectos.map((proyecto) => (
+            <article className='tarjeta_proyectos' key={proyecto.id}>
+              <div>
+                <a href={proyecto.web} target='_blank'>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                <h2 className='title'>{proyecto.name}</h2>
+                </a>
+              </div>
 
-            <div>
-              <a href={proyecto.web} target='_blank'><p className='desc'>{proyecto.desc}</p></a>
-            </div>
+              <div>
+                <a href={proyecto.web} target='_blank'><p className='desc'>{proyecto.desc}</p></a>
+              </div>
 
-            <div className='divider'>
-            
-            </div>
+              <div className='divider'>
+              
+              </div>
 
-            <div>
-              <p className='tech'>{proyecto.tech}</p>
-            </div>
+              <div>
+                <p className='tech'>{proyecto.tech}</p>
+              </div>
 
-            <div>
-              <p className='year'>{proyecto.year}</p>
-            </div>
-          </article>
-        ))}
-    </div>
+              <div>
+                <p className='year'>{proyecto.year}</p>
+              </div>
+            </article>
+          ))}
+      </div>
     </>
   )
 }
