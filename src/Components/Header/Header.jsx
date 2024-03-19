81452b647b4b03df82d2466e5c103cca7ac6df50
Cristian Mozetich <cristianmozetich@gmail.com>
2024-03-19 20:40:13 +0000

    actualizaciones

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,13 @@ import { useContexto } from "../../Context/Context";
 import "./Header.css";
 
 const Header = () => {
-  const { handleClickMenu, hamburguesa, cambiarIdioma, handleClickLight, idioma } =
-    useContexto();
-
+  const {
+    handleClickMenu,
+    hamburguesa,
+    cambiarIdioma,
+    handleClickLight,
+    idioma,
+  } = useContexto();
 
   const traducciones = {
     ingles: {
@@ -28,7 +32,6 @@ const Header = () => {
       CvEspaño: "CV Español",
       CvIngles: "CV Inglés",
       CvItaliano: "CV Italiano",
-
     },
 
     italiano: {
@@ -40,8 +43,8 @@ const Header = () => {
       CvEspaño: "CV Spagnolo",
       CvIngles: "CV Inglés",
       CvItaliano: "CV Italiano",
-    }
-  }
+    },
+  };
 
   const traduccionActual = traducciones[idioma] || traducciones["ingles"];
 
@@ -50,52 +53,74 @@ const Header = () => {
       <button className="btn_light" onClick={handleClickLight}>
         <i className="fa-solid fa-circle-half-stroke"></i>Navy / Light
       </button>
-      {hamburguesa ?
+      {hamburguesa ? (
         <button className="btn_exit" onClick={handleClickMenu}>
           <i class="fa-solid fa-xmark"></i>
-        </button> :
+        </button>
+      ) : (
         <button className="btn_menu" onClick={handleClickMenu}>
           <i className="fa-solid fa-bars"></i>
         </button>
-      }
+      )}
       {hamburguesa ? (
         <>
-        <div className="backdrop" onClick={handleClickMenu}></div>
-        <div className="menu_hamburguesa">
-          <div>
-            <h4 className="submenu_titulo">{traduccionActual.Modo}</h4>
-            <a className="btn_submenu" onClick={handleClickLight}>
-              <i className="fa-solid fa-circle-half-stroke"></i>Navy / Light
-            </a>
+          <div className="backdrop" onClick={handleClickMenu}></div>
+          <div className="menu_hamburguesa">
+            <div>
+              <h4 className="submenu_titulo">{traduccionActual.Modo}</h4>
+              <a className="btn_submenu" onClick={handleClickLight}>
+                <i className="fa-solid fa-circle-half-stroke"></i>Navy / Light
+              </a>
+            </div>
+            <div className="idiomas">
+              <h4 className="submenu_titulo">{traduccionActual.Lenguaje}</h4>
+              <a
+                className="btn_submenu"
+                onClick={() => cambiarIdioma("español")}
+              >
+                {traduccionActual.Español}
+              </a>
+              <a
+                className="btn_submenu"
+                onClick={() => cambiarIdioma("ingles")}
+              >
+                {traduccionActual.Ingles}
+              </a>
+              <a
+                className="btn_submenu"
+                onClick={() => cambiarIdioma("italiano")}
+              >
+                {traduccionActual.Italiano}
+              </a>
+            </div>
+            <div className="curriculums">
+              <h4 className="submenu_titulo">Curriculums</h4>
+              <a
+                href="/assets/pdf/Cv_Mozetich_Cristian.pdf"
+                target="_blank"
+                className="btn_submenu"
+              >
+                <i class="fa-regular fa-file"></i>
+                {traduccionActual.CvEspaño}
+              </a>
+              <a
+                href="/assets/pdf/Cv_Mozetich_Cristian_EN.pdf"
+                target="_blank"
+                className="btn_submenu"
+              >
+                <i class="fa-regular fa-file"></i>
+                {traduccionActual.CvIngles}
+              </a>
+              <a
+                href="/assets/pdf/Cv_Mozetich_Cristian_IT.pdf"
+                target="_blank"
+                className="btn_submenu"
+              >
+                <i class="fa-regular fa-file"></i>
+                {traduccionActual.CvItaliano}
+              </a>
+            </div>
           </div>
-          <div className="idiomas">
-            <h4 className="submenu_titulo">{traduccionActual.Lenguaje}</h4>
-            <a
-              className="btn_submenu"
-              onClick={() => cambiarIdioma("español")}
-            >
-              {traduccionActual.Español}
-            </a>
-            <a
-              className="btn_submenu"
-              onClick={() => cambiarIdioma("ingles")}
-            >
-              {traduccionActual.Ingles}
-            </a>
-            <a
-              className="btn_submenu"
-              onClick={() => cambiarIdioma("italiano")}
-            >
-              {traduccionActual.Italiano}
-            </a>
-          </div>
-          <div className="curriculums">
-            <h4 className="submenu_titulo">Curriculums</h4>
-            <a href="/assets/pdf/Cv_Mozetich_Cristian.pdf" target="_blank" className="btn_submenu">{traduccionActual.CvEspaño}</a>
-            <a href="/assets/pdf/Cv_Mozetich_Cristian_EN.pdf" target="_blank" className="btn_submenu">{traduccionActual.CvIngles}</a>
-            <a href="/assets/pdf/Cv_Mozetich_Cristian_IT.pdf" target="_blank" className="btn_submenu">{traduccionActual.CvItaliano}</a>
-          </div>
-        </div>
         </>
       ) : null}
     </header>
